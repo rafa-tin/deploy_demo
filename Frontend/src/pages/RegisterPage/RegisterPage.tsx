bf946716a84f143bcc86b884b45c02c5738ee4c6
rafa-tin <146753352+rafa-tin@users.noreply.github.com>
2025-07-06 10:46:44 +0500

    Update RegisterPage.tsx

--- Frontend/src/pages/RegisterPage/RegisterPage.tsx
+++ Frontend/src/pages/RegisterPage/RegisterPage.tsx
@@ -33,7 +33,7 @@ const RegistrationPage: React.FC = () => {
   const onSubmit = async (data: RegistrationPropsInput) => {
   try {
     const cleanPhone = data.phoneNumber.replace(/\D/g, ''); 
-    const response = await fetch('https://f386-31-148-166-6.ngrok-free.app/api/auth/sign-up', {
+    const response = await fetch('https://8118-31-148-166-6.ngrok-free.app/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
