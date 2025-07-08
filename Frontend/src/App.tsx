f4f5b46c917ff71b5db999677a2d5931b40293b2
rafa-tin <146753352+rafa-tin@users.noreply.github.com>
2025-07-08 17:26:13 +0500

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainPage from './pages/MainPage/MainPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
